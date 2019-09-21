d97d3a7fb14020980eb2f64ff1e3becb4deb63cb
mateus4k <mateus4k@protonmail.ch>
2019-09-20 23:44:02 -0300

    :rocket: Recuperação de senha

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,5 +1,6 @@
-'use strict';
+'use strict'
 
+const moment = require('moment')
 const crypto = require('crypto')
 const User = use('App/Models/User')
 const Mail = use('Mail')
@@ -34,6 +35,34 @@ class ForgotPasswordController {
         error: {
           message: 'Something went wrong. Does this email really exist?'
         }
+      })
+    }
+  }
+
+  async update ({ request, response }) {
+    try {
+      const { token, password } = request.all()
+
+      const user = await User.findByOrFail('token', token)
+
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at)
+
+      if (tokenExpired) {
+        return response.status(401).send({
+          error: { message: 'Reset password token has expired.' }
+        })
+      }
+
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+
+      await user.save()
+    } catch (err) {
+      return response.status(err.status).send({
+        error: { message: 'Something went wrong with your password reset.' }
       })
     }
   }
--- start/routes.js
+++ start/routes.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
@@ -6,3 +6,4 @@ const Route = use('Route')
 Route.post('users', 'UserController.store')
 Route.post('sessions', 'SessionController.store')
 Route.post('passwords', 'ForgotPasswordController.store')
+Route.put('passwords', 'ForgotPasswordController.update')
