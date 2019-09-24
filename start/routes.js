012a9922dd4ebe1d8909d952caf6e7aa7cd836d6
Mateus <mateus4k@protonmail.ch>
2019-09-23 22:20:00 -0300

    :rocket: Validations

--- start/routes.js
+++ start/routes.js
@@ -1,17 +1,17 @@
 'use strict';
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
-const Route = use('Route')
+const Route = use('Route');
 
-Route.post('users', 'UserController.store')
-Route.post('sessions', 'SessionController.store')
+Route.post('users', 'UserController.store').validator('User');
+Route.post('sessions', 'SessionController.store');
 
-Route.post('passwords', 'ForgotPasswordController.store')
-Route.put('passwords', 'ForgotPasswordController.update')
+Route.post('passwords', 'ForgotPasswordController.store');
+Route.put('passwords', 'ForgotPasswordController.update');
 
-Route.get('files/:id', 'FileController.show')
+Route.get('files/:id', 'FileController.show');
 Route.group(() => {
-  Route.post('files', 'FileController.store')
-  Route.resource('projects', 'ProjectController').apiOnly()
-  Route.resource('projects.tasks', 'TaskController').apiOnly()
-}).middleware(['auth'])
+  Route.post('files', 'FileController.store');
+  Route.resource('projects', 'ProjectController').apiOnly();
+  Route.resource('projects.tasks', 'TaskController').apiOnly();
+}).middleware(['auth']);
