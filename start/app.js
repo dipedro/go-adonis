55c662b07dd78da503193f5cc7da65da44bbbc86
Mateus <mateus4k@protonmail.ch>
2019-09-23 22:43:37 -0300

    :rocket: Adonis internationalization :rocket:

--- start/app.js
+++ start/app.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 /*
  * |--------------------------------------------------------------------------
@@ -18,8 +18,9 @@ const providers = [
   '@adonisjs/lucid/providers/LucidProvider',
   '@adonisjs/mail/providers/MailProvider',
   '@adonisjs/framework/providers/ViewProvider',
-  '@adonisjs/validator/providers/ValidatorProvider'
-];
+  '@adonisjs/validator/providers/ValidatorProvider',
+  '@adonisjs/antl/providers/AntlProvider'
+]
 
 /*
  * |--------------------------------------------------------------------------
@@ -30,7 +31,7 @@ const providers = [
  * | Providers for migrations, tests etc.
  * |
  */
-const aceProviders = ['@adonisjs/lucid/providers/MigrationsProvider'];
+const aceProviders = ['@adonisjs/lucid/providers/MigrationsProvider']
 
 /*
  * |--------------------------------------------------------------------------
@@ -44,7 +45,7 @@ const aceProviders = ['@adonisjs/lucid/providers/MigrationsProvider'];
  * |   { Route: 'Adonis/Src/Route' }
  * |
  */
-const aliases = {};
+const aliases = {}
 
 /*
  * |--------------------------------------------------------------------------
@@ -54,6 +55,6 @@ const aliases = {};
  * | Here you store ace commands for your package
  * |
  */
-const commands = [];
+const commands = []
 
-module.exports = { providers, aceProviders, aliases, commands };
+module.exports = { providers, aceProviders, aliases, commands }
